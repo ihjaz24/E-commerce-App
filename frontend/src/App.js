c2ecb8e9ccd4600277ca9a8f2dfb0bdbe0d94ac4
Ihjazul Aslam AT <ihjazmohd19@gmail.com>
2023-09-02 14:53:09 +0530

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Layout from './components/layout/Layout'
-
+import {Routes,Route} from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import HomePage from './pages/HomePage';
+import Pagenotfound from './pages/Pagenotfound';
+import Policy from './pages/Policy';
 function App() {
   return ( 
     <>
-    <Layout>
-      <h1>Ecommerce app</h1>
-    </Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='*' element={<Pagenotfound />} />
+
+
+        
+      </Routes> 
     </>
   );
 }
