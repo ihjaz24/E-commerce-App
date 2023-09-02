2ccb24ddc202e604f88906b79b9303a07f5aea82
Ihjazul Aslam AT <ihjazmohd19@gmail.com>
2023-09-02 17:16:56 +0530

    header footer

--- frontend/src/pages/Pagenotfound.js
+++ frontend/src/pages/Pagenotfound.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import Layout from '../components/layout/Layout'
+
 
 const Pagenotfound = () => {
   return (
     <Layout>
-      <h1>Page not found</h1>
+      <div className='pageNotFound'>
+        <h1 className='pageNotFound-title'>404</h1>
+        <h2 className='pageNotFound-heading'>Page not found</h2>
+        <Link to='/' className='pageNotFound-btn' > Go Back</Link>
+      </div>
     </Layout>
   )
 }
