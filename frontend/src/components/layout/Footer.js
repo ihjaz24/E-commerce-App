b02d12e335230d3ed23ab7cc3dcb4cb353368a34
Ihjazul Aslam AT <ihjazmohd19@gmail.com>
2023-09-02 14:30:27 +0530

    frontend configuration

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        <h5 className='text-center'>
+            All Right reserved &copy; ihjaz 
+        </h5>
+    </div>
+  )
+}
+
+export default Footer
